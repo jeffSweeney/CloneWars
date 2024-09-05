41a34dbabf1e70a77850287eb57434cdbe9fcb11
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-09-05 16:29:03 -0400

    Project organization and color sets

--- CalculatorClone/CalculatorClone/Core/Calculator/View/CalculatorView.swift
+++ CalculatorClone/CalculatorClone/Core/Calculator/View/CalculatorView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  CalculatorView.swift
 //  CalculatorClone
 //
 //  Created by Jeffrey Sweeney on 9/5/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CalculatorView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    CalculatorView()
 }
