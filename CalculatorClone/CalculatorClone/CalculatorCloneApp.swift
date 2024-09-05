8985dca72ac1bd72e8b2c0eb9f45af71151e40a2
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-09-05 15:43:57 -0400

    Initial blank app

---
+++ CalculatorClone/CalculatorClone/CalculatorCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorCloneApp.swift
+//  CalculatorClone
+//
+//  Created by Jeffrey Sweeney on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
