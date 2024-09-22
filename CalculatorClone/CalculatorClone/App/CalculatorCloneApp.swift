22e7c696464ec5076f3499def90fc28e98ca9d91
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-09-22 17:17:22 -0400

    Integrate SweeneyCloneWarsIntro package into app init

--- CalculatorClone/CalculatorClone/App/CalculatorCloneApp.swift
+++ CalculatorClone/CalculatorClone/App/CalculatorCloneApp.swift
@@ -5,13 +5,24 @@
 //  Created by Jeffrey Sweeney on 9/5/24.
 //
 
+import SweeneyCloneWarsIntro
 import SwiftUI
 
 @main
 struct CalculatorCloneApp: App {
+    @State var showingIntro = true
+    
+    private let episode: UInt8 = 1
+    private let appName = "iPhone Calculator"
+    
     var body: some Scene {
         WindowGroup {
-            CalculatorView()
+            if showingIntro {
+                SweeneyCloneWarsIntro(showingIntro: $showingIntro, episodeNumber: episode, appName: appName)
+            } else {
+                CalculatorView()
+            }
         }
     }
 }
+
