73d1e5c3514b1f48b0edd14c052af27348d3e2d9
jeffSweeney <92553377+jeffSweeney@users.noreply.github.com>
2024-10-05 10:11:16 -0400

    Project initialization

---
+++ ContactsClone/ContactsClone/ContactsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsCloneApp.swift
+//  ContactsClone
+//
+//  Created by Jeffrey Sweeney on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ContactsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
